5d5480af8cd155ea68ac85ccb7422068ffaa7d73
Jason Shultz <jasshultz@gmail.com>
2015-09-18 08:42:47 -0600

    init commit

--- FindAPrimeNumber/ViewController.swift
+++ FindAPrimeNumber/ViewController.swift
@@ -10,6 +10,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblResult: UILabel!
+    
+    @IBOutlet weak var txtInput: UITextField!
+    
+    @IBAction func btnSubmit(sender: AnyObject) {
+        
+        if (txtInput.text != nil) {
+            
+            let number:Int = Int(txtInput.text!)!
+            
+            if isPrime(number) {
+                lblResult.text = "\(number) a prime number"
+            } else {
+                lblResult.text = "\(number) not a prime number"
+            }
+            
+        }
+        
+    }
+    
+    func isOdd(number: Int) -> Bool {
+        if number % 2 == 1 {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func divides(a: Int, b: Int) -> Bool {
+        return a % b == 0
+    }
+    
+    func countDivisors(number: Int) -> Int {
+        var cnt = 0
+        for i in 1...number {
+            if divides(number, b: i) {
+                ++cnt
+            }
+        }
+        return cnt
+    }
+    
+    func isPrime(number: Int) -> Bool {
+        return countDivisors(number) == 2
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
